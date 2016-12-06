a833a51779d7a610f6e84ea58ea0cf4246b3c3dc
Catalin Bostan <catalin.bostan@eadesign.ro>
2016-12-06 17:44:07 +0200

    Added the buttons on Edit side of Stock Items and I made the xml for Edit Stock Items

---
+++ Controller/Adminhtml/StockItems/Newitem.php
@@ -0,0 +1,66 @@
+<?php
+/*******************************************************************************
+ *
+ * EaDesign
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License (AFL 3.0)
+ * that is bundled with this package in the file LICENSE_AFL.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/afl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@eadesign.ro so we can send you a copy immediately.
+ *
+ * @category    eadesigndev_warehouses
+ * @copyright   Copyright (c) 2008-2016 EaDesign by Eco Active S.R.L.
+ * @license     http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+ *
+ ******************************************************************************/
+
+namespace Eadesigndev\Warehouses\Controller\Adminhtml\StockItems;
+
+class Newtitem extends \Eadesigndev\Warehouses\Controller\Adminhtml\StockItems
+{
+    /**
+     * @var \Magento\Backend\Model\View\Result\Forward
+     */
+    private $resultForwardFactory;
+
+    /**
+     * @param \Magento\Backend\App\Action\Context $context
+     * @param \Magento\Backend\Model\View\Result\ForwardFactory $resultForwardFactory
+     */
+    public function __construct(
+        \Magento\Backend\App\Action\Context $context,
+        \Magento\Framework\Registry $coreRegistry,
+        \Magento\Backend\Model\View\Result\ForwardFactory $resultForwardFactory
+    )
+    {
+        $this->resultForwardFactory = $resultForwardFactory;
+        parent::__construct($context, $coreRegistry);
+    }
+
+    /**
+     * Forward to edit
+     *
+     * @return \Magento\Backend\Model\View\Result\Forward
+     */
+    public function execute()
+    {
+        /** @var \Magento\Backend\Model\View\Result\Forward $resultForward */
+        $resultForward = $this->resultForwardFactory->create();
+        return $resultForward->forward('edit');
+    }
+
+    /**
+     * Check the permission to run it
+     *
+     * @return boolean
+     */
+    protected function _isAllowed()
+    {
+        return $this->_authorization->isAllowed(self::ADMIN_RESOURCE_VIEW);
+    }
+}
---
+++ Ui/Component/Listing/ColumnItems/Options.php
@@ -0,0 +1,72 @@
+<?php
+/*******************************************************************************
+ *
+ * EaDesign
+ *  
+ * NOTICE OF LICENSE
+ *  
+ * This source file is subject to the Academic Free License (AFL 3.0)
+ * that is bundled with this package in the file LICENSE_AFL.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/afl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@eadesign.ro so we can send you a copy immediately.
+ *  
+ * @category    eadesigndev_warehouses
+ * @copyright   Copyright (c) 2008-2016 EaDesign by Eco Active S.R.L.
+ * @license     http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+ *  
+ ******************************************************************************/
+
+/**
+ * Sample_News extension
+ * /**
+ * EaDesgin
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License (AFL 3.0)
+ * that is bundled with this package in the file LICENSE_AFL.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/afl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@eadesign.ro so we can send you a copy immediately.
+ *
+ * @category    eadesigndev_pdfgenerator
+ * @copyright   Copyright (c) 2008-2016 EaDesign by Eco Active S.R.L.
+ * @license     http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+ */
+
+namespace Eadesigndev\Warehouses\Ui\Component\Listing\ColumnItems;
+
+use Magento\Store\Ui\Component\Listing\Column\Store\Options as StoreOptions;
+
+class Options extends StoreOptions
+{
+
+    /**
+     * All Store Views value
+     */
+    const ALL_STORE_VIEWS = '0';
+
+    /**
+     * Get options
+     *
+     * @return array
+     */
+    public function toOptionArray()
+    {
+//        exit('est');
+        if ($this->options !== null) {
+            return $this->options;
+        }
+
+        $this->generateCurrentOptions();
+
+        $this->options = array_values($this->currentOptions);
+
+        return $this->options;
+    }
+}
